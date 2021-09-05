51ea49c398a0ece4e9ba34fe745641a6a2bff620
Jon <22411356+jjhna@users.noreply.github.com>
2021-09-05 01:04:50 -0400

    Lesson 2 Updated notes
    Lesson 2 Updated notes:
    
    Added in notes in the total-spent-by-customer.py to show how the reducebykey() function really works by:
    
    In this specific example the reducebykey function takes a lambda function. It takes 2 arguments, which represents two values that have the SAME key so we calculate the sum of the two values and return it to a single value to the SAME key.
    
    and how to sort a key, value pair by only sorting through the key by flipping the pair.

--- total-spent-by-customer-sorted.py
+++ total-spent-by-customer-sorted.py
@@ -13,6 +13,10 @@ totalByCustomer = mappedInput.reduceByKey(lambda x, y: x + y)
 
 #Changed for Python 3 compatibility:
 #flipped = totalByCustomer.map(lambda (x,y):(y,x))
+
+# ImportantNOTE: In order to sort a value from the largest or smallest in a key, value pair
+# you can only perform this action by sorting by the key not the value, so in this case we need to flip the key, value pair
+# so that we can sort the flipped value by the key to get our results
 flipped = totalByCustomer.map(lambda x: (x[1], x[0]))
 
 totalByCustomerSorted = flipped.sortByKey()
--- total-spent-by-customer.py
+++ total-spent-by-customer.py
@@ -9,6 +9,10 @@ def extractCustomerPricePairs(line):
 
 input = sc.textFile("file:///sparkcourse/customer-orders.csv")
 mappedInput = input.map(extractCustomerPricePairs)
+# https://youtu.be/RE_L6silAJE?t=145
+# In this specific example the reducebykey function takes a lambda function 
+# It takes 2 arguments, which represents two values that have the SAME key
+# so we calculate the sum of the two values and return it to a single value to the SAME key
 totalByCustomer = mappedInput.reduceByKey(lambda x, y: x + y)
 
 results = totalByCustomer.collect();
